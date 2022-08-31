430cad2b6f23711c61a3de4b939a5096a40647de
Patrick M. Ryan <patrick.ryan@evanschambers.com>
2022-08-31 16:20:15 -0400

    cleaned up

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -1,9 +1,8 @@
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as cdk from 'aws-cdk-lib';
-// import BundlingOptions from 'aws-cdk-lib';
 import * as path from 'path';
-import { join } from 'path';
+import { join, resolve } from 'path';
 
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -23,34 +22,22 @@ export class CdkStarterStack extends cdk.Stack {
     const layer_dir = 'layers/skyfield'
     const container_dir = '/install'
 
-    // const dockerVolume = cdk.DockerVolume = {
-    //   containerPath: container_dir,
-    //   hostPath: join(__dirname, layer_dir) ,
-    // });
-
     const skyfieldLayer = new lambda.LayerVersion(this, 'skyfield-layer', {
       compatibleRuntimes: [lambda.Runtime.PYTHON_3_9],
-      // code: lambda.Code.fromAsset('src/layers/calc'),
-
       code: lambda.Code.fromAsset('layers/skyfield', {
         bundling: {
           image: new cdk.DockerImage('python:3.9-slim'), // lambda.Runtime.PYTHON_3_9.bundlingImage,
-          // volumes: [dockerVolume],
           volumes: [
-            // cdk.DockerVolume(
             {
               containerPath: container_dir,
-              hostPath:
-                '/Users/pmryan/ec/projects/galactica/aws-cdk-lambda-layers/layers/skyfield',
+              hostPath: resolve(layer_dir),
+                // '/Users/pmryan/ec/projects/galactica/aws-cdk-lambda-layers/layers/skyfield',
                 // join(__dirname, layer_dir),
             }
             // ),
           ],
           // command: 'pip install'+opts+' -r '+container_dir +'/requirements.txt -t /asset-output/python',
           command: [
-            // 'bash',
-            // '-c',
-            // 'echo',
             'pip',
             'install',
             '--disable-pip-version-check',
