e3de46b802a2cf785e277a18b9a082ba829fe4c6
Patrick M. Ryan <patrick.ryan@evanschambers.com>
2022-08-31 16:07:33 -0400

    working-ish

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -1,7 +1,9 @@
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as cdk from 'aws-cdk-lib';
+// import BundlingOptions from 'aws-cdk-lib';
 import * as path from 'path';
+import { join } from 'path';
 
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -16,6 +18,55 @@ export class CdkStarterStack extends cdk.Stack {
       code: lambda.Code.fromAsset('src/layers/calc'),
       description: 'multiplies a number by 2',
     });
+
+    const opts = '--disable-pip-version-check --no-cache-dir'
+    const layer_dir = 'layers/skyfield'
+    const container_dir = '/install'
+
+    // const dockerVolume = cdk.DockerVolume = {
+    //   containerPath: container_dir,
+    //   hostPath: join(__dirname, layer_dir) ,
+    // });
+
+    const skyfieldLayer = new lambda.LayerVersion(this, 'skyfield-layer', {
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_9],
+      // code: lambda.Code.fromAsset('src/layers/calc'),
+
+      code: lambda.Code.fromAsset('layers/skyfield', {
+        bundling: {
+          image: new cdk.DockerImage('python:3.9-slim'), // lambda.Runtime.PYTHON_3_9.bundlingImage,
+          // volumes: [dockerVolume],
+          volumes: [
+            // cdk.DockerVolume(
+            {
+              containerPath: container_dir,
+              hostPath:
+                '/Users/pmryan/ec/projects/galactica/aws-cdk-lambda-layers/layers/skyfield',
+                // join(__dirname, layer_dir),
+            }
+            // ),
+          ],
+          // command: 'pip install'+opts+' -r '+container_dir +'/requirements.txt -t /asset-output/python',
+          command: [
+            // 'bash',
+            // '-c',
+            // 'echo',
+            'pip',
+            'install',
+            '--disable-pip-version-check',
+            '--no-cache-dir',
+            '-r',
+            join(container_dir, '/requirements.txt'),
+            '-t',
+            '/asset-output/python',
+          ],
+          // network: 'host'
+        },
+      }),
+      description: 'skyfield',
+
+      });
+
 
     // 👇 3rd party library layer
     const yupLayer = new lambda.LayerVersion(this, 'yup-layer', {
